dba4a535a85255498ca4a9b6b57d24e34589a0cf
Your Name <movcan77@gmail.com>
2021-08-13 12:48:08 +0300

    Adding bean annotation for change file size for upload

--- src/main/java/com/example/onlinelibrary/OnlineLibraryApplication.java
+++ src/main/java/com/example/onlinelibrary/OnlineLibraryApplication.java
@@ -2,9 +2,22 @@ package com.example.onlinelibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class OnlineLibraryApplication {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.parse("4096KB"));
+        factory.setMaxRequestSize(DataSize.parse("4096KB"));
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineLibraryApplication.class, args);
